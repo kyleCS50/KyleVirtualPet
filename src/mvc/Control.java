1cedab94e3e95f40723db7c6ecba7b246631dc2d
kyleCS50 <kyledanielfrancis@gmail.com>
2021-09-26 13:43:27 +1300

    Fixed MVC test
    Fixed issues with the mvc test. Implemented simple name and age labels and field and combo box.

--- src/mvc/Control.java
+++ src/mvc/Control.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package mvc;
 
 import java.awt.event.ActionEvent;
@@ -15,19 +15,20 @@ import java.awt.event.ActionListener;
 public class Control implements ActionListener{
     private Model model;
     private View view;
-
+    
     public Control(Model model, View view) {
         this.model = model;
         this.view = view;
         
         view.getSaveButton().addActionListener(this);
     }
-
+    
     @Override
-    public void actionPerformed(ActionEvent arg0) {
-        view.getNameLabel().setText(view.getNameField().getText());
-        System.out.println("saved");
+    public void actionPerformed(ActionEvent ae) {
+        view.getNameLabel().setText("Name: "+view.getNameField().getText());
+        view.getAgeLabel().setText("Age: "+view.getAgeBox().getSelectedItem());
         view.updateUI();
+        
     }
     
     
--- src/mvc/View.java
+++ src/mvc/View.java
@@ -6,6 +6,7 @@
 package mvc;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -24,78 +25,90 @@ public class View extends JPanel{
     private JTextField nameField;
     private JComboBox ageBox;
     private JButton saveButton;
+    private JPanel inputPanel;
+    private JPanel outputPanel;
+    private Integer[] ageArray;
     
     public View(String title)
     {
-        //set up frame
-        this.frame = new JFrame(title);
-        frame.getContentPane().setLayout(new BorderLayout());
-        frame.getContentPane().setLayout(new BorderLayout());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 120);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
         
         //create elements
         this.nameLabel = new JLabel("Name: ");
         this.ageLabel = new JLabel("Age: ");
-        this.nameField = new JTextField();
-        this.ageBox = new JComboBox();
+        this.nameField = new JTextField(10);
         this.saveButton = new JButton("Save Info");
+        this.inputPanel = new JPanel();
+        this.outputPanel = new JPanel();
+        this.ageArray = new Integer[50];
+        for(int i = 0; i < ageArray.length; i++)
+        {
+            this.ageArray[i] = (i+1);
+        }
+        this.ageBox = new JComboBox(ageArray);
         
-        this.add(nameLabel, BorderLayout.CENTER);
-        this.add(ageLabel, BorderLayout.CENTER);
-        this.add(nameField, BorderLayout.NORTH);
-        this.add(ageBox, BorderLayout.NORTH);
-        this.add(saveButton, BorderLayout.NORTH);
+        outputPanel.add(nameLabel);
+        outputPanel.add(ageLabel);
+        outputPanel.setBackground(Color.RED);
+        inputPanel.add(nameField);
+        inputPanel.add(ageBox);
+        inputPanel.add(saveButton);
+        inputPanel.setBackground(Color.BLUE);
+        this.add(outputPanel, BorderLayout.NORTH);
+        this.add(inputPanel, BorderLayout.SOUTH);
         
-        frame.add(this);
+        //frame setup
+        this.frame = new JFrame(title);
+        frame.getContentPane().add(this);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 120);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
-
+    
     public JFrame getFrame() {
         return frame;
     }
-
+    
     public void setFrame(JFrame frame) {
         this.frame = frame;
     }
-
+    
     public JLabel getNameLabel() {
         return nameLabel;
     }
-
+    
     public void setNameLabel(JLabel nameLabel) {
         this.nameLabel = nameLabel;
     }
-
+    
     public JLabel getAgeLabel() {
         return ageLabel;
     }
-
+    
     public void setAgeLabel(JLabel ageLabel) {
         this.ageLabel = ageLabel;
     }
-
+    
     public JTextField getNameField() {
         return nameField;
     }
-
+    
     public void setNameField(JTextField nameField) {
         this.nameField = nameField;
     }
-
+    
     public JComboBox getAgeBox() {
         return ageBox;
     }
-
+    
     public void setAgeBox(JComboBox ageBox) {
         this.ageBox = ageBox;
     }
-
+    
     public JButton getSaveButton() {
         return saveButton;
     }
-
+    
     public void setSaveButton(JButton saveButton) {
         this.saveButton = saveButton;
     }
