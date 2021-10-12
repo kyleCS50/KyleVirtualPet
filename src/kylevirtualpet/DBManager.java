81352d358890217a50fa4e81a5c4a89ba85ce0b7
kyleCS50 <kyledanielfrancis@gmail.com>
2021-10-13 12:56:11 +1300

    Remove VirtualPetApp
    remove old CUI interface for game

--- src/kylevirtualpet/DBManager.java
+++ src/kylevirtualpet/DBManager.java
@@ -23,11 +23,6 @@ public final class DBManager {
         establishConnection();
     }
     
-    public static void main(String[] args) {
-        DBManager dbManager = new DBManager();
-        System.out.println(dbManager.getConnection());
-    }
-    
     public Connection getConnection() {
         return this.conn;
     }
